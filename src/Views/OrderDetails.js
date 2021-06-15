05087c8159fdd24935f3616809a0e8b7168e5f6e
Rothery275 <szymon96mazur@gmail.com>
2021-06-15 23:09:02 +0200

    details added

--- src/Views/OrderDetails.js
+++ src/Views/OrderDetails.js
@@ -1,11 +1,24 @@
 import { useEffect, useState } from 'react';
 import { useParams } from 'react-router';
+import { makeStyles, Paper, Typography, TextField } from '@material-ui/core';
 import fireDB from '../Firebase';
 import MainWrapper from '../Components/MainWrapper/MainWrapper';
+import moment from 'moment';
+
+const useStyles = makeStyles((theme) => ({
+  label: {
+    textTransform: 'uppercase',
+    color: theme.palette.grey[600],
+  },
+  paper: {
+    padding: theme.spacing(3),
+  },
+  button: {},
+}));
 
 const OrderDetails = (props) => {
   const { taskId } = useParams();
-  console.log(taskId);
+  const classes = useStyles();
   const [taskDetail, setTaskDetail] = useState();
 
   useEffect(() => {
@@ -27,7 +40,47 @@ const OrderDetails = (props) => {
   return (
     <div>
       <MainWrapper>
-        <div></div>
+        {taskDetail && (
+          <Paper className={classes.paper}>
+            <Typography className={classes.label} variant="body1">
+              Zleceniodawca
+            </Typography>
+            <Typography gutterBottom variant="body2">
+              {taskDetail.customer}
+            </Typography>
+            <Typography className={classes.label} variant="body1">
+              Sygnatura sprawy
+            </Typography>
+            <Typography gutterBottom variant="body2">
+              {taskDetail.signature}
+            </Typography>
+            <Typography className={classes.label} variant="body1">
+              Data utworzenia
+            </Typography>
+            <Typography gutterBottom variant="body2">
+              {moment(taskDetail.createDate).format('DD-MM-YYYY')}
+            </Typography>
+            <Typography className={classes.label} variant="body1">
+              Data odbioru
+            </Typography>
+            <Typography gutterBottom variant="body2">
+              {moment(taskDetail.pickupDate).format('DD-MM-YYYY')}
+            </Typography>
+            <Typography className={classes.label} variant="body1">
+              Odbiorca
+            </Typography>
+            <Typography gutterBottom variant="body2">
+              {taskDetail.employee}
+            </Typography>
+            <TextField
+              label="dodatkowe informacje"
+              value={taskDetail.notes}
+              onChange={() => {
+                console.log('działa');
+              }}
+            />
+          </Paper>
+        )}
       </MainWrapper>
     </div>
   );
