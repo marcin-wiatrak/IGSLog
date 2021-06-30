852f46be90651c655975ece750bf193f7ff4124e
Rothery275 <szymon96mazur@gmail.com>
2021-06-30 18:36:17 +0200

    password reset added, customer adding fixed

--- src/UserProfile.js
+++ src/UserProfile.js
@@ -5,10 +5,12 @@ import {
   Modal,
   Paper,
   makeStyles,
+  Snackbar,
 } from '@material-ui/core';
 import { useContext, useState } from 'react';
 import { AuthContext } from './Auth';
 import fireDB from './Firebase';
+import { Alert } from '@material-ui/lab';
 
 const useStyles = makeStyles((theme) => ({
   paper: {
@@ -19,21 +21,25 @@ const useStyles = makeStyles((theme) => ({
     left: '50%',
     padding: theme.spacing(3),
   },
+  modalFooter: {
+    marginTop: theme.spacing(2),
+  },
 }));
 
 const UserProfile = () => {
   const classes = useStyles();
   const { currentUserProfile } = useContext(AuthContext);
   const [confirmationModalOpen, setConfirmationModalOpen] = useState(false);
-  const [snackbar, setSnackbar] = useState();
+  const [snackbar, setSnackbar] = useState('');
 
   const handleResetPassword = async () => {
     try {
       await fireDB.auth().sendPasswordResetEmail(currentUserProfile.email);
-      setSnackbar(true);
       setConfirmationModalOpen(false);
+      setSnackbar('OK');
     } catch (error) {
       console.log('error', error);
+      setSnackbar('ERROR');
     }
   };
 
@@ -44,6 +50,21 @@ const UserProfile = () => {
         <Button onClick={() => setConfirmationModalOpen(true)}>
           Zmiana hasła
         </Button>
+        <Snackbar
+          open={!!snackbar}
+          anchorOrigin={{
+            vertical: 'top',
+            horizontal: 'right',
+          }}
+          onClose={() => setSnackbar('')}
+          autoHideDuration={6000}
+        >
+          {snackbar === 'OK' && (
+            <Alert severity="success">
+              Mail resetujący hasło został wysłany na Twój adres email!
+            </Alert>
+          )}
+        </Snackbar>
       </MainWrapper>
       <Modal
         open={confirmationModalOpen}
@@ -58,16 +79,22 @@ const UserProfile = () => {
             Na Twój adres email: {`${currentUserProfile.email}`} zostanie
             wysłany link resetujący hasło.
           </Typography>
-          <Button
-            color="primary"
-            variant="contained"
-            onClick={handleResetPassword}
-          >
-            POTWIERDŹ
-          </Button>
-          <Button onClick={() => setConfirmationModalOpen(false)}>
-            ANULUJ
-          </Button>
+          <div className={classes.modalFooter}>
+            <Button
+              color="primary"
+              variant="contained"
+              onClick={handleResetPassword}
+            >
+              POTWIERDŹ
+            </Button>
+            <Button
+              onClick={() => {
+                setConfirmationModalOpen(false);
+              }}
+            >
+              ANULUJ
+            </Button>
+          </div>
         </Paper>
       </Modal>
     </>
