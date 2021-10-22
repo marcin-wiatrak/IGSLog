21c9556e77d3054be7cb2279d64a566ea3a8bec3
Marcin Wiatrak <mwiatrak@gmail.com>
2021-10-22 17:42:01 +0200

    update 1

--- src/Views/Calendar.js
+++ src/Views/Calendar.js
@@ -1,9 +1,9 @@
 import MainWrapper from '../Components/MainWrapper/MainWrapper';
 import moment from 'moment';
 import clsx from 'clsx';
-import { makeStyles, Paper, Typography } from '@material-ui/core';
-import { Notes } from '@material-ui/icons';
-
+import { IconButton, makeStyles, Paper, Typography } from '@material-ui/core';
+import { ArrowBack, ArrowForward, Notes } from '@material-ui/icons';
+import { useEffect, useState } from 'react';
 
 const useStyles = makeStyles((theme) => ({
   paper: {
@@ -13,7 +13,9 @@ const useStyles = makeStyles((theme) => ({
   heading: {
     fontWeight: 'bold',
   },
-  calendar: {},
+  calendar: {
+    maxWidth: 840,
+  },
   calendarWeek: {
     display: 'flex',
   },
@@ -22,7 +24,7 @@ const useStyles = makeStyles((theme) => ({
     padding: 6,
     width: 120,
     height: 120,
-    border: '1px solid black',
+    border: `1px solid ${theme.palette.grey[300]}`,
   },
   calendarLabel: {
     textTransform: 'uppercase',
@@ -53,34 +55,60 @@ const useStyles = makeStyles((theme) => ({
       fontSize: 10,
     },
   },
+  calendarControls: {
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  calendarToday: {
+    backgroundColor: theme.palette.primary.light,
+  },
 }));
+
+const weekDaysLabels = [
+  'Poniedziałek',
+  'Wtorek',
+  'Środa',
+  'Czwartek',
+  'Piątek',
+  'Sobota',
+  'Niedizela',
+];
 
 const Calendar = () => {
   const classes = useStyles();
+  const [calendar, setCalendar] = useState([]);
+  const [today, setToday] = useState(moment());
 
-  const today = moment();
-  const startDay = today.clone().startOf('month').startOf('week').isoWeekday(2);
-  const endDay = today.clone().endOf('month').endOf('week');
-  const day = startDay.clone().subtract(2, 'day');
-  const calendar = [];
+  const currentMonth = today.clone().format('MMMM');
+  const currentYear = today.clone().format('YYYY');
 
-  const weekDaysLabels = [
-    'Poniedziałek',
-    'Wtorek',
-    'Środa',
-    'Czwartek',
-    'Piątek',
-    'Sobota',
-    'Niedizela',
-  ];
+  const prevMonth = () => {
+    return today.clone().startOf('month').subtract(1, 'month');
+  };
 
-  while (day.isBefore(endDay, 'day')) {
-    calendar.push(
-      Array(7)
-        .fill(0)
-        .map(() => day.add(1, 'day').clone())
-    );
-  }
+  const nextMonth = () => {
+    return today.clone().startOf('month').add(1, 'month');
+  };
+
+  useEffect(() => {
+    const startDay = today
+      .clone()
+      .startOf('month')
+      .startOf('week')
+      .isoWeekday(2);
+    const endDay = today.clone().endOf('month').endOf('week');
+    const day = startDay.clone().subtract(2, 'day');
+    const tempCalendar = [];
+    while (day.isBefore(endDay, 'day')) {
+      tempCalendar.push(
+        Array(7)
+          .fill(0)
+          .map(() => day.add(1, 'day').clone())
+      );
+    }
+    setCalendar(tempCalendar);
+  }, [today]);
 
   return (
     <MainWrapper>
@@ -94,6 +122,15 @@ const Calendar = () => {
       </Typography>
       <Paper className={classes.paper}>
         <div className={classes.calendar}>
+          <div className={classes.calendarControls}>
+            <IconButton onClick={() => setToday(prevMonth())}>
+              <ArrowBack />
+            </IconButton>
+            <Typography variant="h6">{`${currentMonth.toUpperCase()} ${currentYear}`}</Typography>
+            <IconButton onClick={() => setToday(nextMonth())}>
+              <ArrowForward />
+            </IconButton>
+          </div>
           <div className={classes.calendarWeek}>
             {weekDaysLabels.map((label) => (
               <div className={classes.calendarLabel}>{label}</div>
@@ -112,13 +149,20 @@ const Calendar = () => {
                         classes.calendarDay,
                         classes.outOfCurrentMonth
                       )}
+                      onClick={() => setToday(day)}
                     >
                       {day.format('D').toString()}
                     </div>
                   );
                 }
                 return (
-                  <div className={classes.calendarDay}>
+                  <div
+                    className={clsx(
+                      classes.calendarDay,
+                      today.isSame(day, 'day') && classes.calendarToday
+                    )}
+                    onClick={() => setToday(day)}
+                  >
                     <div className={classes.dayLabel}>
                       {day.format('D').toString()}
                     </div>
@@ -129,7 +173,7 @@ const Calendar = () => {
                         <Typography variant="body2">Do: 5</Typography>
                         <Typography variant="body2">Pnd: 5</Typography>
                       </div>
-                      <div className={classes.dayCOntentNotes}>
+                      <div className={classes.dayContentNotes}>
                         <Notes />
                       </div>
                     </div>
